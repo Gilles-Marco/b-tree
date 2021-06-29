1cbe99e89fc6b3e6423cdc3c205cc38dcaf77b18
cguibourg-58 <42938472+cguibourg-58@users.noreply.github.com>
2021-06-29 12:26:16 +0200

    Add code for 2.2

--- src/fr/miage/fsgbd/MainNoGUI.java
+++ src/fr/miage/fsgbd/MainNoGUI.java
@@ -34,12 +34,66 @@ public class MainNoGUI {
         }
         System.out.println("Data successfully loaded !");
 
-        KeyValue<Integer, FrenchIdentity> result = bInt.rechercheArbre(5400);
+        /*KeyValue<Integer, FrenchIdentity> result = bInt.rechercheArbre(5400);
         System.out.println(result.getValue());
 
         result = bInt.rechercheSequentielle(5400);
 //        KeyValue<Integer, FrenchIdentity> result = bInt.rechercheSequentielle(5400);
-        System.out.println(result.getValue());
+        System.out.println(result.getValue());*/
+        //j'ai commenté parce que ça fait planter l'application quand j'essaye avec avec un nombre de clef differents de 10000
+        
+        
+        //test de temps
+        //1: rechercheArbre()
+        long startTime = System.nanoTime();
+        long minTime = 147483647;
+        long maxTime = 0;
+        for(int i=0; i<100; i++) {
+            Integer val = (int)(Math.random() * (10000-1));
+            long startTimeTmmp = System.nanoTime();
+            bInt.rechercheArbre(val);
+            long endTimeTmp = System.nanoTime();
+            long timeElapsedTmp = endTimeTmp - startTimeTmmp;
+            if(maxTime<timeElapsedTmp) {
+                maxTime = timeElapsedTmp;
+            }
+            if(minTime>timeElapsedTmp){
+                minTime = timeElapsedTmp;
+            }
+        }
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        long moyenneParRecherche = timeElapsed/100;
+        System.out.println();
+        System.out.println("rechercheArbre() : " + moyenneParRecherche/1000 + " microsecondes d'execution en moyenne !");
+        System.out.println("temps minimum : " + minTime/1000 + " microsecondes d'execution");
+        System.out.println("temps maximum : " + maxTime/1000 + " microsecondes d'execution");
+
+        //test de temps()
+        //2: rechercheSequencielle
+        startTime = System.nanoTime();
+        minTime = 147483647;
+        maxTime = 0;
+        for(int i=0; i<100; i++) {
+            Integer val = (int)(Math.random() * (10000-1));
+            long startTimeTmmp = System.nanoTime();
+            bInt.rechercheSequentielle(val);
+            long endTimeTmp = System.nanoTime();
+            long timeElapsedTmp = endTimeTmp - startTimeTmmp;
+            if(maxTime<timeElapsedTmp) {
+                maxTime = timeElapsedTmp;
+            }
+            if(minTime>timeElapsedTmp){
+                minTime = timeElapsedTmp;
+            }
+        }
+        endTime = System.nanoTime();
+        timeElapsed = endTime - startTime;
+        moyenneParRecherche = timeElapsed/100;
+        System.out.println();
+        System.out.println("rechercheSequentielle() : " + moyenneParRecherche/1000 + " microsecondes d'execution en moyenne !");
+        System.out.println("temps minimum : " + minTime/1000 + " microsecondes d'execution");
+        System.out.println("temps maximum : " + maxTime/1000 + " microsecondes d'execution");
 
     }
 
